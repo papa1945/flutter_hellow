2b1f845dfc863a9536d7d64e0e443fe53ecb9704
papa1945 <papa1945@gmail.com>
2023-01-03 18:12:13 +0900

    first commit flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Center(
+            child: Container(
+                width: 50, height: 50, color: Color.fromARGB(255, 230, 7, 7))));
+  }
+}
