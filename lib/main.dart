c754afea12b84cc33cae0b45d59eeccca64a42d5
papa1945 <papa1945@gmail.com>
2023-01-04 16:06:39 +0900

    [study]coustom 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,39 @@ void main() {
   runApp(const MyApp());
 }
 
+class bottombar extends StatelessWidget {
+  const bottombar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black87,
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            Icon(
+              Icons.phone,
+              color: Colors.white,
+            ),
+            Icon(Icons.message, color: Colors.white),
+            Icon(Icons.contact_page, color: Colors.white)
+          ]),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Center(
-            child: Container(
-                width: 50, height: 50, color: Color.fromARGB(255, 230, 7, 7))));
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Bio1. 현장 점검 시트'),
+              backgroundColor: Colors.black87,
+            ),
+            body: Text('Hellow World'),
+            bottomNavigationBar: bottombar()));
   }
 }
